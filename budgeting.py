9f6efd9525d03feb05dd93398522e358de7031ba
Will Grieger <dividify@outlook.com>
2026-06-04 09:06:38 -0500

    adding prompt to budgeting

--- budgeting.py
+++ budgeting.py
@@ -1,7 +1,27 @@
+import openpyxl
 import subprocess
 
-subprocess.run('ollama, llama3.1')
 
+prompt = '''
+You will be passed credit card or similiar transaction data as a row of comma seperated values.
+
+Your role will be to clean the data and assign a category. 
+
+The categories are:
+
+
+
+Please review the tranaction data and return it in this format:
+
+"[transaction date],[transaction vendor (where the money was spent)],[amount of spend as positive number (how much money was spent)],
+[assigned category]"
+
+
+'''
+
+result = subprocess.run(['ollama', 'run', 'llama3.1',prompt],capture_output=True, text=True)
+
+print(result.stdout)
 
 # Set folder location of main budget document
 
