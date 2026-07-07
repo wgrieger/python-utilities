36b18ce7af2595be945f336a2be634d956b8680d
Will Grieger <dividify@outlook.com>
2026-07-07 16:51:33 -0500

    updating prompt

--- budgeting.py
+++ budgeting.py
@@ -1,4 +1,3 @@
-import subprocess
 import os
 import pandas as pd
 from google import genai
@@ -13,38 +12,20 @@ client = genai.Client(api_key=os.environ.get('PYTHON_GEMINI_KEY'))
 
 budgetExcelDoc = os.path.expanduser("~/Documents/Personal Finance/Budgeting/Grieger Personal Budget Tracker.xlsm")
 
-# with open_xlsx(budgetExcelDoc) as wb:
-#     workSheet= wb['2026']
 
 rawTransactionFolder = os.path.expanduser("~/Documents/Personal Finance/Budgeting/Raw Transaction Data")
-
-# which worksheet within budget excel doc? 
-# define 2026 somehow 
 
 #------------------
 # Prompt and Function for LLM
 #------------------
 
 prompt = '''
-Take this row of data and determine if it is a credit card transaction. 
+ Here is a spreadsheets of credit card transactions. Look at each row and follow these instructions\
+carefully.
 
-If it is not a credit card transaction or you believe it to be a payment, say '0' and add no further commentary to your response.
+If it is not a credit card transaction or you believe it to be a payment, ignore the data.
 
-If it is a credit card transaction, match it to one of the categories below, and respond with the transaction formatted exactly like this,
-and with no other commentary in the response:
-
-'[transaction date],[cleaned and formatted vendor name],[transaction amount as positive number],[assigned category]'
-
-Rules:
-- Don't include the brackets or quotes in your response
-- Don't ever add any text that doesn't fit the the response criteria provided, including in an initital response
-- Use the context provided in the data to aid your categorization
-- The response is being processed by code, so it is imperitive it is structured exactly as ordered and never deviates at all.
-    i.e. do not respond anything like 'Please provide the row of data you would like me to process.'
-- If data is blank, return 0
-
-
-Allowed Categories:
+If it is a credit card spending transaction, match it to one of the categories below:
 - Dining: Restaurants, delivery apps, bars, food courts.
 - Gas/Automotive: Gas stations, car maintenance, dealerships, auto parts stores.
 - Merchandise: General retail, Amazon, online shopping orders not fitting elsewhere.
@@ -59,14 +40,22 @@ Allowed Categories:
 - Charity: Donations and non-profit contributions.
 - Interest / Fees: Card annual fees, bank fees, interest charges.
 
-Here is the data to process:
+Rules:
+- Don't ever add any text or characters that don't fit the the response criteria provided, including in an initial response
+- Use the context provided in the data to aid your categorization
+- it is imperative it is structured exactly as ordered and never deviates at all.
+i.e. do not respond anything like 'Please provide the row of data you would like me to process.'
+- Date format 'MM-DD-YY'
+- Include credits and refunds where applicable as a negative number
+- accuracy is very important here
+- brackets in format are used for example and should not be present in final output
+
+
+Finally, return as text in the CSV format all of the transactions with this exact structure:
+
+'[transaction date],[cleaned and formatted vendor name],[transaction amount as positive number],[assigned category]' 
 '''
 
-# def llmQuery(transaction):
-#     input = prompt + transaction 
-#     query = subprocess.run(['ollama', 'run', 'llama3.1',input],capture_output=True, text=True)
-#     output = query.stdout
-#     return output
 
 def geminiQuery(transaction):
     input = prompt + transaction
